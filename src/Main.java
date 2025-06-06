2cd3df9904eb6b4c5ef97a57cb83cb3274c29e50
Polina Bodnar <honey@list.ru>
2025-06-06 21:33:58 +0200

    Домашнее задание №11

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-// Исключения из заданий
-class NegativeDepositException extends Exception {
-    public NegativeDepositException(String message) {
-        super(message);
-    }
-}
-
-class ItemNotFoundException extends RuntimeException {
-    public ItemNotFoundException(String message) {
-        super(message);
-    }
-}
-
-class LoginFailedException extends Exception {
-    public LoginFailedException(String message) {
-        super(message);
-    }
-}
-
-class InvalidTransferAmountException extends Exception {
-    public InvalidTransferAmountException(String message) {
-        super(message);
-    }
-}
-
-class InsufficientBalanceException extends Exception {
-    public InsufficientBalanceException(String message) {
-        super(message);
-    }
-}
-
-class InvalidRatingException extends Exception {
-    public InvalidRatingException(String message) {
-        super(message);
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         // 1. Безопасное деление
