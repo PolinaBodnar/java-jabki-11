adade76bb1859bdeb1a5754f378ea55ef72faf39
Polina Bodnar <honey@list.ru>
2025-06-25 15:18:05 +0200

    Обновление

--- homework/exception/InvalidRatingException.java
+++
@@ -1,7 +0,0 @@
-package homework.exception;
-
-public class InvalidRatingException extends Exception{
-    public InvalidRatingException() {
-        super("Рейтинг должен быть от 1 до 5");
-    }
-}
--- homework/exception/ItemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package homework.exception;
-
-public class ItemNotFoundException extends Exception {
-    public ItemNotFoundException() {
-        super("Товар с данным кодом не найден.");
-    }
-}
